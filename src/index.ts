40277093b1051ff657baace04a29ce3e6ab821b5
David Nelson <david.nelson1@uqconnect.edu.au>
2024-12-13 10:20:23 +1000

    First successful postman http api call to update an online database EVER

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import * as dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import router from './router';
 
 dotenv.config();
 
@@ -26,4 +28,10 @@ server.listen(PORT, () => {
 });
 
 const MONGODB_ATLAS_PASSWORD = process.env.MONGODB_ATLAS_PASSWORD
-const MONGO_URL = `mongodb+srv://davidhomecube:${MONGODB_ATLAS_PASSWORD}@cluster0.guwvp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const MONGO_URL = `mongodb+srv://david:${MONGODB_ATLAS_PASSWORD}@cluster0.guwvp.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
